db7158747d468919c47f1d26d33842bdea14c107
ennenbach <ennenbach@worthandlung.de>
2018-01-23 11:19:50 -0500

    :sparkles: Verify facebook webhook

--- entrypoint/fb.js
+++ entrypoint/fb.js
@@ -1,7 +1,23 @@
-module.exports.main = (event, context, callback) => {
-    console.log(event);
+module.exports.verify = (event, context, callback) => {
+  const params = event.queryStringParameters || {};
+
+  const token = params['hub.verify_token'];
+  const challenge = params['hub.challenge'];
+  const mode = params['hub.mode'];
+
+  if ((mode && token && challenge) &&
+      (mode === 'subscribe') &&
+      (token === process.env.FB_VERIFYTOKEN)
+     ) {
     callback(null, {
-        statusCode: 200,
-        body: 'Hallo Welt!',
+    statusCode: 200,
+    body: challenge,
     });
-}
+    return;
+  }
+
+  callback(null, {
+  statusCode: 400,
+  body: 'Parameter missing'
+  });
+};
