686aec3ea2ea50739ba4f718ea373c34646c2ed4
Marcus Weiner <marcus.weiner@gmail.com>
2018-01-26 16:36:10 +0100

    :recycle: Move fragment output handler to lib

--- handler/payload_report_start.js
+++ handler/payload_report_start.js
@@ -1,15 +1,12 @@
 const request = require('request');
-
-const url = id => `${process.env.CMS_API_URL}reports/${id}/?withFragments=1`;
+const urls = require('../lib/urls');
+const fragmentSender = require('../lib/fragmentSender');
 
 const report_start = fbLib => (psid, payload) => {
-    request(url(payload.report), (error, res, body) => {
-
+    request(`${urls.report(payload.report)}?withFragments=1`, (error, res, body) => {
         const report = JSON.parse(body);
-        console.log(report);
 
-        fbLib.sendTextMessage(psid, report.text);
-
+        fragmentSender(psid, report.next_fragments, payload, report.text);
     })
 };
 
