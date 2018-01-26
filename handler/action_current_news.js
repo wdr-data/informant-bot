fcfc60c4c3896d8385c67d6b59fa57d5556b9721
Jannes Höke <jhoeke@uni-bremen.de>
2018-01-26 12:51:26 +0100

    :sparkles: Send headlines with button

--- handler/action_current_news.js
+++ handler/action_current_news.js
@@ -1,4 +1,4 @@
-const request = require('request') 
+const request = require('request');
 
 const url = `${process.env.CMS_API_URL}pushes/?limit=1`;
 
@@ -6,11 +6,23 @@ const current_news = fbLib => psid => {
   request(url, (error, res, body) => {
     const data = JSON.parse(body);
     console.log(data);
+
     const push = data.results[0];
     fbLib.sendTextMessage(psid, push.intro);
+
     const headlines = push.reports.map(r => r.headline).join(' +++ ');
-    fbLib.sendTextMessage(psid, headlines);
-  })  
-}
+    const firstReport = push.reports[0];
+    const button = {
+        type: 'postback',
+        title: 'Los geht\'s',
+        payload: JSON.stringify({
+            action: 'report_start',
+            push: push.id,
+            report: firstReport.id,
+        }),
+    };
+    fbLib.sendButtonsMessage(psid, headlines, [button]);
+  })
+};
 
 module.exports = current_news;
