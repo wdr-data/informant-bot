f4b1b26443b2850a46671ce2cf722ca683bb88e1
EC2 Default User <ec2-user@ip-172-31-11-208.eu-west-1.compute.internal>
2018-01-30 18:16:44 +0000

    :construction_worker: Use s3 to aquire runtime environment vars in CI

--- config/env.js
+++ config/env.js
@@ -1,6 +1,8 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
 const path = require('path');
+const S3 = require('aws-sdk/clients/s3');
+const request = require('request-promise-native');
 
 const required_env = [
     'FB_PAGETOKEN',
@@ -9,7 +11,33 @@ const required_env = [
     'CMS_API_URL',
 ];
 
+const load_s3 = (filename, json = false) => {
+    const branch = process.env.BRANCH;
+    const s3client = new S3({ region:'eu-central-1' });
+    const uri = s3client.getSignedUrl('getObject', {
+        Bucket: 'wdr-tim-bot-env',
+        Key: `${branch}/${filename}`,
+    });
+    return request({ uri, json })
+        .catch(err => {
+            throw new Error(`Fetching env from S3 failed with: ` +
+                            `${err.name === 'StatusCodeError' ? err.statusCode : err.message}`);
+        });
+};
+module.exports.load_s3 = load_s3;
+
 const fetch_env = () => {
+    if ('CI' in process.env){
+        return load_s3('env.json', true)
+            .then(env => {
+                const missing_env = required_env.filter(name => !(name in env));
+                if (missing_env.length > 0) {
+                    throw new Error(`Missing environment variables: ${missing_env.join(', ')}`);
+                }
+                return env;
+            });
+    }
+
     const dotenv_path = path.resolve(__dirname, "../.env.yml");
     const environment = {};
     if(fs.existsSync(dotenv_path)) {
@@ -29,8 +57,8 @@ const fetch_env = () => {
 
     return environment;
 };
+module.exports.env = fetch_env;
 
 module.exports.stage = () => {
     return process.env.SLS_STAGE || fetch_env()['DEPLOY_ALIAS'] || 'dev';
 };
-module.exports.env = fetch_env;
--- config/prepare-ci.js
+++ config/prepare-ci.js
@@ -2,15 +2,24 @@
 
 const fs = require('fs');
 const path = require('path');
+const { load_s3 } = require('./env');
 
 const df_service_account = () => {
+    const save_file = content => fs.writeFile(
+        path.resolve(__dirname, '../.df_id.json'),
+        content,
+        { encoding: 'UTF-8' },
+        () => console.log("DF Service Account written.")
+    );
+
+    if ('CI' in process.env){
+        return load_s3('df_id.json').then(save_file);
+    }
+
     if(!('DF_SERVICE_ACCOUNT' in process.env)) {
         throw new Error("DF Service Account JSON is missing!");
     }
-
-    const content = process.env.DF_SERVICE_ACCOUNT;
-    const filePath = path.resolve(__dirname, '../.df_id.json');
-    fs.writeFile(filePath, content, { encoding: 'UTF-8' }, () => console.log("DF Service Account written."));
+    save_file(process.env.DF_SERVICE_ACCOUNT);
 };
 
 const prepare = function() {
