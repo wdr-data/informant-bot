c3e110449c119f120f35075123608ecc9ab0a4a7
Jannes Höke <jhoeke@uni-bremen.de>
2018-02-28 17:06:13 +0100

    :sparkles: Add implementation for Broadcast API and user labeling

--- handler/index.js
+++ handler/index.js
@@ -1,7 +1,6 @@
 
 module.exports = {
     actions: {
-<<<<<<< HEAD
         current_time: require('./action_current_time'),
         current_news: require('./action_current_news'),
     },
@@ -9,15 +8,5 @@ module.exports = {
         report_start: require('./payload_report_start'),
         fragment_next: require('./payload_fragment_next'),
         pushOutro: require('./payloadPushOutro'),
-=======
-        current_time: require('./action_current_time')(fbLib),
-        current_news: require('./action_current_news')(fbLib),
-        subscribe: require('./payload_subscribe')(fbLib),
-    },
-    payloads: {
-        report_start: require('./payload_report_start')(fbLib),
-        fragment_next: require('./payload_fragment_next')(fbLib),
-        subscribe: require('./payload_subscribe')(fbLib),
->>>>>>> :sparkles: Trigger subscribe feature by intent and payload
     },
 };
