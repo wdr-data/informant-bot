e3bd566f33a0256b7da38c529133777f69ac7c71
LizzAy <lisachenbach@gmx.de>
2018-03-01 11:30:56 +0100

    :sparkles: Send list to subscribe for pushes

--- handler/payload_subscribe.js
+++ handler/payload_subscribe.js
@@ -2,36 +2,63 @@ const { buttonPostback, listElement } = require('../lib/facebook');
 const request = require('request');
 const urls = require('../lib/urls');
 
-const subscribe = fbLib => (psid, payload) => {
-    fbLib.sendTextMessage(psid, "Wenn du magst, bringe ich dich zwei Mal am Tag auf den neuesten Stand. Hier kannst du die Benachrichtigungen aktivieren und deaktivieren:")
-    const button = {
-        type: 'postback',
-        title: 'beides',
-        payload: JSON.stringify({
-            action: 'subscribe',
-            subscriptions: 'all',
-        }),
-    };
-    const elements = [
-        {
-          action: 'subscribe',
-          subscriptions: 'all',
-        });
-    console.log('button: ', button);
+module.exports = function (chat, payload) {
+    if (payload.subscription) {
+        subscriptionChange(chat, payload);
+        return;
+    }
+
+    chat.sendText("Wenn du magst, bringe ich dich zwei Mal am Tag auf den neuesten Stand. Hier kannst du die Benachrichtigungen aktivieren und deaktivieren:");
 
     const elements = [];
     elements.push(listElement(
-        'Dein Update am morgen',
-        'So, gegen 7:30Uhr gibt\'s die ersten Infos'
+        'Dein Update am Morgen',
+        'Gegen 7:30 Uhr gibt\'s die ersten Infos.',
+        buttonPostback(
+            'Anmelden',
+            {
+                action: 'subscribe',
+                subscription: 'morning',
+            }
+        )
     ));
 
     elements.push(listElement(
-        'Abends sind die News noch besser!',
-        '18:30Uhr ist ok? Kannste haben!',
+        'Dein Update am Abend',
+        'Um 18:30 Uhr kommt alles was am Tag interessant war.',
+        buttonPostback(
+            'Anmelden',
+            {
+                action: 'subscribe',
+                subscription: 'evening',
+            }
+        )
+    ));
+
+    elements.push(listElement(
+        'Immer Up-to-date',
+        'Ich informiere dich um 7:30 Uhr und um 18:30 Uhr.',
+        buttonPostback(
+            'Anmelden',
+            {
+                action: 'subscribe',
+                subscription: 'all',
+            }
+        )
     ));
 
     console.log('elements: ', elements);
-    chat.sendList(elements, button);
+    chat.sendList(elements);
 };
 
-module.exports = subscribe;
+function subscriptionChange (chat, payload) {
+    let time = '';
+    if (payload.subscription == 'morning') {
+        time = '7:30 Uhr';
+    } else if (payload.subscription == 'evening') {
+        time = '18:30 Uhr';
+    } else {
+        time = '7:30 Uhr und um 18:30 Uhr';
+    }
+    chat.sendText(`Alles klar! Täglich um ${time} gibt es dein Update.`);
+}
