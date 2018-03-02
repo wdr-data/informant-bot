7793025f493660b661c478b30c6720e29d33eeb3
Jannes Höke <jhoeke@uni-bremen.de>
2018-03-02 17:53:06 +0100

    :sparkles: Send outro media

--- handler/payloadPushOutro.js
+++ handler/payloadPushOutro.js
@@ -5,6 +5,15 @@ const pushOutro = (chat, payload) => {
     request(`${urls.push(payload.push)}`, (error, res, body) => {
         const push = JSON.parse(body);
 
+        if (push.media) {
+            chat.sendAttachment(push.media).then(() => {
+                chat.sendText(push.outro);
+            }).catch(error => {
+                console.log('Sending outro media failed', error)
+            });
+            return;
+        }
+
         chat.sendText(push.outro);
     });
 };
