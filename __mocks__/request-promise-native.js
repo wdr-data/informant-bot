cd864f04f04f311ae4fa6a710168db5e8cc339e0
Jannes Höke <jhoeke@uni-bremen.de>
2018-03-21 09:34:29 +0100

    :page_facing_up: Add Copyright notice for request mock and Apache 2 License text

--- __mocks__/request-promise-native.js
+++ __mocks__/request-promise-native.js
@@ -1,3 +1,33 @@
+// Modified version of the "request" libraries "index.js"
+// https://github.com/request/request/blob/v2.85.1/index.js
+//
+// Changes:
+//  - Mock requests by hashing request parameters and loading responses from
+//    a file instead of doing network operations
+//  - Copied helper function "paramsHaveRequestBody" from
+//    https://github.com/request/request/blob/v2.85.1/lib/helpers.js
+//  - Minor changes to coding style
+//
+// The original license is provided as "LICENSE_APACHE2" with this software.
+//
+//
+// --- Original Copyright Notice ---
+//
+// Copyright 2010-2012 Mikeal Rogers
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+
 const extend = require('extend');
 const hash = require('object-hash');
 const fs = require('fs');
