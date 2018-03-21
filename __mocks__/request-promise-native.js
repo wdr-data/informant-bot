236f305711017ecbe87729b8408b837f291d0f98
christinegotth <christine.gotth@gmail.com>
2018-03-21 14:36:14 +0100

    :sparkles: Allow expecting requests in tests

--- __mocks__/request-promise-native.js
+++ __mocks__/request-promise-native.js
@@ -32,6 +32,11 @@ const extend = require('extend');
 const hash = require('object-hash');
 const fs = require('fs');
 
+const calls = [];
+beforeEach(() => {
+  calls.length = 0;
+});
+
 // organize params for patch, post, put, head, del
 function initParams (uri, options, callback) {
   if (typeof options === 'function') {
@@ -73,6 +78,8 @@ function request (uri, options, callback) {
     }
     const paramsHash = hash(params);
 
+    calls.push(params);
+
     fs.readFile(`./__mockData__/${paramsHash}.json`, 'utf8', (err, data) => {
       if (err) {
         reject(`Reading mock request file for parameters ` +
@@ -84,7 +91,7 @@ function request (uri, options, callback) {
         resolve(JSON.parse(data));
       } else {
         resolve(data);
-      }      
+      }
     })
   });
 }
@@ -107,5 +114,6 @@ request.put = verbFunc('put');
 request.patch = verbFunc('patch');
 request.del = verbFunc('delete');
 request['delete'] = verbFunc('delete');
+request.calls = calls;
 
 module.exports = request;
