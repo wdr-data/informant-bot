d5ad357c3e678448c9ff958e12d025ca2b8cdbf5
p3nny <p3nny@hacking.studio>
2018-03-22 14:01:55 +0000

    :bug: Use promises correctly

--- handler/payload_faq.js
+++ handler/payload_faq.js
@@ -1,21 +1,20 @@
-const request = require('request');
+const request = require('request-promise-native');
 const urls = require('../lib/urls');
 const fragmentSender = require('../lib/fragmentSender');
 
 const faq_start = (chat, payload) => {
     const url = `${urls.faqBySlug(payload.slug)}`;
 
-    request(url, (error, res, body) => {
-        const faq = JSON.parse(body);
-        
+    return request({uri: url, json: true}).then( faq => {
+
         if (typeof faq[0] == 'undefined') {
             chat.sendText(`Dazu habe ich noch keine Info...🤔`);
             return;
         }
 
         payload.type = payload.action;
-        fragmentSender(chat, faq[0].next_fragments, payload, faq[0].text, faq[0].media);
-    })
+        return fragmentSender(chat, faq[0].next_fragments, payload, faq[0].text, faq[0].media);
+    });
 };
 
 module.exports = faq_start;
