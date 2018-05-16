ca352cc25839b733674119f3799d36353ab84f17
ChristianJoe <cjoe@gmx.de>
2018-05-16 12:54:25 +0200

    :art: Return promise indicating when faq sent

--- handler/actionFaq.js
+++ handler/actionFaq.js
@@ -2,7 +2,5 @@ import payloadFaq from './payloadFaq';
 
 export default function(slug) {
     const payload = { action: 'faq', slug };
-    return function(chat) {
-        payloadFaq(chat, payload);
-    };
+    return (chat) => payloadFaq(chat, payload);
 }
