2e91fe5d7741700e522e7230cfcbec0a2dd67e1c
LizzAy <lisachenbach@gmx.de>
2018-05-16 12:51:01 +0200

    :fire: Remove unused code

--- handler/__tests__/actionCurrentNews.test.js
+++ handler/__tests__/actionCurrentNews.test.js
@@ -77,22 +77,5 @@ describe('actionCurrentNews', () => {
                     push: 4,
                 })
             );
-        /* .buttons(
-            'Hey, alles klar bei dir? Dein Informant ist wieder hier - ' +
-            'und das habe ich für dich:\n' +
-    '➡ Luft in einigen NRW-Städten ist besser geworden\n' +
-    '➡ Unfall löst Diskussion über selbstfahrende Autos aus\n' +
-    '➡ Public Viewing wird auch nach 22 Uhr erlaubt\n' +
-    '➡ Das Letzte: Deine Songs zum Glücklichsein',
-            [
-                facebook.buttonPostback(
-                    'Leg los',
-                    {
-                        action: 'report_start',
-                        push: 4,
-                        report: 2,
-                        type: 'push',
-                    }),
-            ]); */
     });
 });
