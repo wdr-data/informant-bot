6bc9928e2a4c47fcb335f09576e491829d65513b
ChristianJoe <cjoe@gmx.de>
2018-05-16 12:33:41 +0200

    :recycle: Simplify `actionFAQ` by one default function

--- handler/actionFaq.js
+++ handler/actionFaq.js
@@ -1,33 +1,8 @@
 import payloadFaq from './payloadFaq';
 
-export function dataProtection(chat) {
-    const payload = { action: 'faq', slug: 'datenschutz' };
-    payloadFaq(chat, payload);
-    return;
+export default function(slug) {
+    const payload = { action: 'faq', slug };
+    return function(chat) {
+        payloadFaq(chat, payload);
+    };
 }
-
-export function imprint(chat) {
-    const payload = { action: 'faq', slug: 'impressum' };
-    payloadFaq(chat, payload);
-    return;
-}
-
-export function howTo(chat) {
-    const payload = { action: 'faq', slug: 'how_to' };
-    payloadFaq(chat, payload);
-    return;
-}
-
-export function about(chat) {
-    const payload = { action: 'faq', slug: 'about' };
-    payloadFaq(chat, payload);
-    return;
-}
-
-export function onboarding(chat) {
-    const payload = { action: 'faq', slug: 'onboarding' };
-    payloadFaq(chat, payload);
-    return;
-}
-
-export default about;
