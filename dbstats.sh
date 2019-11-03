bab70569f14704e772c310f09ad074f6f2a343a0
Jannes Höke <mail@jhoeke.de>
2019-11-03 22:47:46 +0100

    :bento: Move Analytics query to the end

--- dbstats.sh
+++ dbstats.sh
@@ -2,10 +2,6 @@
 
 table_prefix="wdraktuell-bot-fb-prod"
 
-echo "Analytics enabled: "
-aws dynamodb scan --table-name ${table_prefix}-tracking --filter-expression "enabled = :en" --expression-attribute-values '{":en": {"BOOL": true}}' --select "COUNT" | jq .Count
-echo ""
-
 echo "Morning:"
 aws dynamodb scan --table-name ${table_prefix}-subscriptions --filter-expression "morning = :mo" --expression-attribute-values '{":mo": {"N": "1"}}' --select "COUNT" | jq .Count
 echo ""
@@ -20,3 +16,7 @@ echo ""
 
 echo "Breaking:"
 aws dynamodb scan --table-name ${table_prefix}-subscriptions --filter-expression "breaking = :br" --expression-attribute-values '{":br": {"N": "1"}}' --select "COUNT" | jq .Count
+
+echo "Analytics enabled: "
+aws dynamodb scan --table-name ${table_prefix}-tracking --filter-expression "enabled = :en" --expression-attribute-values '{":en": {"BOOL": true}}' --select "COUNT" | jq .Count
+echo ""
