5b01cbe1633a54b5adb0e5e61cabdaac5755cab6
ChristianJoe <cjoe@gmx.de>
2020-02-14 09:50:16 +0100

    :recycle: Use payload to get timing information

--- handler/payloadReportStart.js
+++ handler/payloadReportStart.js
@@ -17,20 +17,8 @@ export default async (chat, payload) => {
     if (report.is_quiz) {
         payload.quiz = true;
     }
-    if (!payload.link && report.link) {
-        let push = undefined;
-        if (payload.push) {
-            const params = {
-                uri: `${urls.push(payload.push)}`,
-                json: true,
-            };
-            // Authorize so we can access unpublished items
-            if (payload.preview) {
-                params.headers = { Authorization: 'Token ' + process.env.CMS_API_TOKEN };
-            }
-            push = await request(params);
-        }
-        payload.link = trackLink(report, push);
+    if (report.link) {
+        payload.link = trackLink(report, payload.timing);
     }
     if (report.audio) {
         payload.audio = report.audio;
