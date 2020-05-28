73a22a428af027ad0bf2a40113f43038c3068c09
ChristianJoe <cjoe@gmx.de>
2020-05-28 14:15:48 +0200

    :chart_with_upwards_trend: Extend questions with numbers for filtering

--- data/surveyQuestions.js
+++ data/surveyQuestions.js
@@ -1,12 +1,12 @@
 export default [
     {
-        'question': 'Die Themenauswahl fand ich:',
+        'question': '1. Die Themenauswahl fand ich:',
         'answers': [ 'zu wenig relevant', 'zu wenig regional', 'ausgewogen und gut' ],
     }, {
-        'question': 'Nachrichten und Eilmeldungen kamen:',
+        'question': '2. Nachrichten und Eilmeldungen kamen:',
         'answers': [ 'zu oft', 'zu selten', 'genau richtig' ],
     }, {
-        'question': 'Die Sprache des Messengers fand ich:',
+        'question': '3. Die Sprache des Messengers fand ich:',
         'answers': [ 'angenehm', 'zu locker' ],
     },
 ];
