c2845ab7527beb8d26849085fbe67af87dd8b012
ChristianJoe <cjoe@gmx.de>
2020-05-28 14:14:40 +0200

    :card_file_box: Rename dynamoDB

--- config/dynamodb.js
+++ config/dynamodb.js
@@ -124,7 +124,7 @@ const tableProps = {
             },
         ],
     },
-    'UserStates': {
+    'userStates': {
         AttributeDefinitions: [
             {
                 AttributeName: 'psid',
