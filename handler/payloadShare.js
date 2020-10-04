644bc5964a8a7dd790e045d20d182df00b497729
ChristianJoe <cjoe@gmx.de>
2020-10-04 12:37:09 +0200

    ✨ Introduce share pic option for sharing bot with friends

--- handler/payloadShare.js
+++ handler/payloadShare.js
@@ -1,5 +1,18 @@
+import { buttonUrl, genericElement } from '../lib/facebook';
 import { payloadFaq } from './payloadFaq';
 
 export default async function(chat) {
-    return payloadFaq(chat, { slug: 'share' });
+    await payloadFaq(chat, { slug: 'share' });
+
+    const shareUrl = 'https://m.me/wdraktuell?ref=sharingmenu';
+    const title ='Immer auf dem laufenden';
+    const subtitle = 'Kurz und Knapp informiert';
+    const button = buttonUrl('Jetzt starten', shareUrl );
+    const imageUrl = 'https://www1.wdr.de/nachrichten/wdr-aktuell-icon-facebook-mesenger-100~_v-gseapremiumxl.jpg';
+    const defaultAction = {
+        type: 'web_url',
+        url: shareUrl,
+    };
+    const element = genericElement(title, subtitle, button, imageUrl, defaultAction);
+    return chat.sendGenericTemplate([ element ]);
 }
