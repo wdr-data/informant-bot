249029025e44a5dd7b69534423dacbc191682d32
ChristianJoe <cjoe@gmx.de>
2020-10-06 10:47:35 +0200

    🍱 Exchange image with correct ratio 1,91:1

--- handler/payloadShare.js
+++ handler/payloadShare.js
@@ -8,7 +8,7 @@ export default async function(chat) {
     const title ='Immer auf dem laufenden';
     const subtitle = 'Kurz und Knapp informiert';
     const button = buttonUrl('Jetzt starten', shareUrl );
-    const imageUrl = 'https://images.informant.einslive.de/17f48730-e271-4bc0-b2d1-2793118d5c18.png';
+    const imageUrl = 'https://images.informant.einslive.de/ef6b7695-479d-4ab8-a616-ddd61cf5f47d.png';
     const defaultAction = {
         type: 'web_url',
         url: shareUrl,
