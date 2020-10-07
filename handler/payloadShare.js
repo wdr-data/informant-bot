dd56f3a51a68c9bd852eb085ef7308059653eb29
ChristianJoe <cjoe@gmx.de>
2020-10-07 17:09:15 +0200

    💬 Set new claim as title and remove subtitle

--- handler/payloadShare.js
+++ handler/payloadShare.js
@@ -5,14 +5,14 @@ export default async function(chat) {
     await payloadFaq(chat, { slug: 'share' });
 
     const shareUrl = 'https://m.me/wdraktuell?ref=sharingmenu';
-    const title ='Immer auf dem laufenden';
-    const subtitle = 'Kurz und Knapp informiert';
+    const title ='Alles was NRW bewegt';
+    // const subtitle = 'Kurz und Knapp informiert';
     const button = buttonUrl('Jetzt starten', shareUrl );
     const imageUrl = 'https://images.informant.einslive.de/ef6b7695-479d-4ab8-a616-ddd61cf5f47d.png';
     const defaultAction = {
         type: 'web_url',
         url: shareUrl,
     };
-    const element = genericElement(title, subtitle, button, imageUrl, defaultAction);
+    const element = genericElement(title, null, button, imageUrl, defaultAction);
     return chat.sendGenericTemplate([ element ]);
 }
